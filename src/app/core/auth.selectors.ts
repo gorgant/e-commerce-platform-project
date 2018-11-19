e089491a7aa79643ebf30b3e08578b3b03241770
Gorgant CX1 <gorgant@gorgant.com>
2018-11-19 19:35:43 +0800

    implement selectors for isLoggedIn

---
+++ src/app/core/auth.selectors.ts
@@ -0,0 +1,17 @@
+import { createSelector } from '@ngrx/store';
+
+
+// This is known as a feature selector
+// These selector functions take a slice of state (better than using map directly on the store observable)
+// These selector functions have memory and won't calculate new outputs if they are called with the same input arguments
+export const selectAuthState = state => state.auth;
+
+export const isLoggedIn = createSelector(
+  selectAuthState,
+  auth => auth.loggedIn
+);
+
+export const isLoggedOut = createSelector(
+  isLoggedIn,
+  loggedIn => !loggedIn
+);
--- src/app/core/components/bs-navbar/bs-navbar.component.ts
+++ src/app/core/components/bs-navbar/bs-navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { AppState } from 'src/app/reducers';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { Logout } from '../../auth.actions';
+import { Observable } from 'rxjs';
+import { isLoggedIn, isLoggedOut } from '../../auth.selectors';
 
 @Component({
   selector: 'bs-navbar',
@@ -13,12 +15,26 @@ export class BsNavbarComponent implements OnInit {
 
   isNavbarCollapsed = true;
 
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+
   constructor(
     public auth: AuthService,
     private store: Store<AppState>) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+      // These select functions ensure observable only emits on actual changes to auth (vs other state changes)
+      this.isLoggedIn$ = this.store
+      .pipe(
+        select(isLoggedIn)
+      );
+
+      this.isLoggedOut$ = this.store
+      .pipe(
+        select(isLoggedOut)
+      );
+
+    }
 
   logout() {
     console.log('Dispatching Logout to store');
