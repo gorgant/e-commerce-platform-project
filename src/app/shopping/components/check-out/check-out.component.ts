44fc9e729b504ed8a7f88a8d8596702991cb9129
Gorgant CX1 <gorgant@gorgant.com>
2018-12-08 09:03:21 +0800

    implement shopping cart summaryu

--- src/app/shopping/components/check-out/check-out.component.ts
+++ src/app/shopping/components/check-out/check-out.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'check-out',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckOutComponent implements OnInit {
 
-  constructor() { }
+  checkoutForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+
 
   ngOnInit() {
+    this.checkoutForm = this.fb.group({
+      name: ['', Validators.required],
+      address1: ['', Validators.required],
+      address2: [''],
+      city: ['', Validators.required],
+    });
   }
+
+  onSubmit() {
+    console.log('Form submitted', this.checkoutForm.value);
+    this.router.navigate(['../order-success', 'orderid'], { relativeTo: this.route });
+  }
+
+  // These getters are used for easy access in the HTML template
+  get name() { return this.checkoutForm.get('name'); }
+  get address1() { return this.checkoutForm.get('address1'); }
+  get address2() { return this.checkoutForm.get('address2'); }
+  get city() { return this.checkoutForm.get('city'); }
 
 }
--- src/app/shopping/components/shopping-cart-summary/shopping-cart-summary.component.ts
+++ src/app/shopping/components/shopping-cart-summary/shopping-cart-summary.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { RootStoreState, ShoppingCartStoreSelectors } from 'src/app/root-store';
+import { Observable } from 'rxjs';
+import { ShoppingCartItem } from 'src/app/shared/models/shopping-cart-item';
 
 @Component({
   selector: 'shopping-cart-summary',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartSummaryComponent implements OnInit {
 
-  constructor() { }
+  shoppingCartItems$: Observable<ShoppingCartItem[]>;
+  cartItemQuantity$: Observable<number>;
+  cartTotalPrice$: Observable<number>;
+
+  constructor(
+    private store$: Store<RootStoreState.State>
+  ) { }
 
   ngOnInit() {
+
+    // Fetch cart items from store
+    this.shoppingCartItems$ = this.store$.select(ShoppingCartStoreSelectors.selectAllCartItems);
+
+    // Query the cart item quantity
+    this.cartItemQuantity$ = this.store$.select(ShoppingCartStoreSelectors.selectCartItemQuantity);
+
+    // Query the cart total price
+    this.cartTotalPrice$ = this.store$.select(ShoppingCartStoreSelectors.selectCartTotalPrice);
   }
 
 }
