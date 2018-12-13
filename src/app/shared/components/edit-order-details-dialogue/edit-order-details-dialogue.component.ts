d34a8c769acaf367cb55ddb949c1f846797df2e8
Gorgant CX1 <gorgant@gorgant.com>
2018-12-13 10:35:56 +0800

    refactor away subscriptions

--- src/app/shared/components/edit-order-details-dialogue/edit-order-details-dialogue.component.ts
+++ src/app/shared/components/edit-order-details-dialogue/edit-order-details-dialogue.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-import { AppUser } from '../../models/app-user';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import {
   RootStoreState,
@@ -8,42 +7,28 @@ import {
   OrdersStoreActions,
   OrdersStoreSelectors
 } from 'src/app/root-store';
-import { ShoppingCartItem } from '../../models/shopping-cart-item';
 import { OrderStatus } from '../../models/order-status';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { OrderService } from '../../services/order.service';
 import { DeliveryInfo } from '../../models/delivery-info';
-import { take } from 'rxjs/operators';
 import { Order } from '../../models/order';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'edit-order-details-dialogue',
   templateUrl: './edit-order-details-dialogue.component.html',
   styleUrls: ['./edit-order-details-dialogue.component.scss']
 })
-export class EditOrderDetailsDialogueComponent implements OnInit, OnDestroy {
-
-  // appUser$: Observable<AppUser>;
-
-  // shoppingCartItems$: Observable<ShoppingCartItem[]>;
-
-  // openOrderName$: Observable<OrderStatus>;
+export class EditOrderDetailsDialogueComponent implements OnInit {
 
   orderDetailsForm: FormGroup;
   orderStatuses$: Observable<OrderStatus[]>;
 
-  orderSubscription: Subscription;
-  statusSubscription: Subscription;
-  formSubscription: Subscription;
+
 
   constructor(
     private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
     private store$: Store<RootStoreState.State>,
-    private orderService: OrderService,
     private dialogRef: MatDialogRef<EditOrderDetailsDialogueComponent>,
     @Inject(MAT_DIALOG_DATA) private order: Order,
   ) { }
@@ -61,8 +46,8 @@ export class EditOrderDetailsDialogueComponent implements OnInit, OnDestroy {
 
     this.orderStatuses$ = this.store$.select(OrderStatusStoreSelectors.selectAllOrderStatuses);
 
-    this.orderSubscription = this.store$.select(OrdersStoreSelectors.selectOrderById(this.order.orderId))
-      // .pipe(take(1))
+    this.store$.select(OrdersStoreSelectors.selectOrderById(this.order.orderId))
+      .pipe(take(1))
       .subscribe(order => {
         this.orderDetailsForm.patchValue({
           name: order.deliveryData.name,
@@ -74,10 +59,10 @@ export class EditOrderDetailsDialogueComponent implements OnInit, OnDestroy {
         });
       });
 
-    this.formSubscription = this.orderDetailsForm.get('orderStatusId').valueChanges.subscribe(value => {
+    this.orderDetailsForm.get('orderStatusId').valueChanges.subscribe(value => {
       console.log('Set order status triggered');
-      this.statusSubscription = this.store$.select(OrderStatusStoreSelectors.selectOrderStatusById(value))
-        // .pipe(take(1))
+      this.store$.select(OrderStatusStoreSelectors.selectOrderStatusById(value))
+        .pipe(take(1))
         .subscribe(orderStatus => {
           console.log('Patching order status to this value', orderStatus);
           this.orderDetailsForm.patchValue({
@@ -106,34 +91,8 @@ export class EditOrderDetailsDialogueComponent implements OnInit, OnDestroy {
     this.dialogRef.close();
   }
 
-  // // This fires when the Category select field is changed, allowing access to the category object
-  // // Without this, when saving the form, the category name will not populate on the form
-  // setOrderStatus(orderStatusId: string) {
-  //   console.log('Set order status triggered');
-  //   this.storeSubscription = this.store$.select(OrderStatusStoreSelectors.selectOrderStatusById(orderStatusId))
-  //     // .pipe(take(1))
-  //     .subscribe(orderStatus => {
-  //       console.log('Patching order status to this value', orderStatus);
-  //       this.orderDetailsForm.patchValue({
-  //         orderStatusName: orderStatus.orderStatusName
-  //       });
-  //     });
-  // }
-
   close() {
     this.dialogRef.close();
-  }
-
-  ngOnDestroy() {
-    if (this.orderSubscription) {
-      this.orderSubscription.unsubscribe();
-    }
-    if (this.statusSubscription) {
-      this.statusSubscription.unsubscribe();
-    }
-    if (this.formSubscription) {
-      this.formSubscription.unsubscribe();
-    }
   }
 
   // These getters are used for easy access in the HTML template
